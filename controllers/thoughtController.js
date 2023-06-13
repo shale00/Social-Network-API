0cf22e3218042f1813cd9ab28738dc4571cf1bb8
sth <sth0067@gmail.com>
2023-06-13 17:31:24 -0500

    updated getSingleThought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -13,7 +13,7 @@ module.exports = {
   // Get single thought by ID
   async getSingleThought(req, res) {
     try {
-      const thought = await Thought.findOne({ _id: req.params.thoughtId });
+      const thought = await Thought.findOne({ _id: req.params.thoughtId }).populate("reactions").select("-__v");
 
       if (!thought) {
         return res.status(404).json({ message: "No thought with that ID" });
